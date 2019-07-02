783662f52c122dd87bc28ae7019b4100849fc87f
nishimoto <yuichiro.nishimoto@metagen.co.jp>
2019-07-02 10:23:28 +0900

    Update main.py

--- main.py
+++ main.py
@@ -110,7 +110,7 @@ def preprocess_df(df):
 
 
 # いろいろfeature_selectionは試した。
-# X5とX14を除くと0.835666へ。
+# X5とX14を除くと0.836へ。
 def preprocess_df(df):
     df.drop(["ID", "a1", "b1", "b2", "a3", "b3"], axis=1, inplace=True)
     df.drop(["X5", "X14"], axis=1, inplace=True)
